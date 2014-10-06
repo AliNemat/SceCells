25944b468dca1006db6dc9a5a9ae454b3aa27b32
Wenzhao Sun <wsun2@acms-gpu.math.nd.edu>
2014-10-06 17:16:12 -0400

    added some methods for cartilage

--- src/srcGPU/Cartilage.cu
+++ src/srcGPU/Cartilage.cu
@@ -21,36 +21,149 @@ void Cartilage::calculateGrowthDir() {
 	// compute position of midpoint.
 	CVector midPoint = cartPara.growNode1 + cartPara.growNode2;
 	midPoint = midPoint / 2;
-
 	// compute current growth direction.
 	CVector direction = midPoint - cartPara.fixedPt;
-	cartPara.GrowthDir = direction.getUnitVector();
-}
+	cartPara.growthDir = direction.getUnitVector();
 
-void Cartilage::addPoint() {
-	//
-}
-
-void Cartilage::grow(double dt) {
-	//
-
+	cartPara.growthDirNode1 = cartPara.growthDir;
+	cartPara.growthDirNode2 = cartPara.growthDir;
 }
 
 void Cartilage::calculateTotalTorque() {
-
+	cartPara.totalTorque = 0;
 }
 
 void Cartilage::move(double dt) {
+	cartPara.angularSpeed = cartPara.totalTorque / cartPara.moInertia;
 }
 
 Cartilage::Cartilage() {
 	nodes = NULL;
 }
 
-void Cartilage::initialize(SceNodes* nodeInput) {
+void Cartilage::initializeMem(SceNodes* nodeInput) {
+	nodes = nodeInput;
+}
+
+void Cartilage::addPoint1(CVector &nodeBehindPos) {
+	CVector node1GrowthDir = cartPara.growNode1 - nodeBehindPos;
+	// get unit vector
+	node1GrowthDir = node1GrowthDir.getModul();
+	CVector incrementVec = node1GrowthDir * cartPara.growthThreshold;
+	CVector posNew = nodeBehindPos + incrementVec;
+	uint indexNew = cartPara.nodeIndexEnd + nodes->getAllocPara().startPosCart;
+	nodes->getInfoVecs().nodeLocX[indexNew] = posNew.GetX();
+	nodes->getInfoVecs().nodeLocY[indexNew] = posNew.GetY();
+	nodes->getInfoVecs().nodeLocZ[indexNew] = posNew.GetZ();
+	cartPara.growNodeBehind1Index = cartPara.nodeIndexEnd;
+	cartPara.nodeIndexEnd++;
+}
+
+void Cartilage::addPoint2(CVector &nodeBehindPos) {
+	CVector node2GrowthDir = cartPara.growNode2 - nodeBehindPos;
+	// get unit vector
+	node2GrowthDir = node2GrowthDir.getModul();
+	CVector incrementVec = node2GrowthDir * cartPara.growthThreshold;
+	CVector posNew = nodeBehindPos + incrementVec;
+	uint indexNew = cartPara.nodeIndexEnd + nodes->getAllocPara().startPosCart;
+	nodes->getInfoVecs().nodeLocX[indexNew] = posNew.GetX();
+	nodes->getInfoVecs().nodeLocY[indexNew] = posNew.GetY();
+	nodes->getInfoVecs().nodeLocZ[indexNew] = posNew.GetZ();
+	cartPara.growNodeBehind2Index = cartPara.nodeIndexEnd;
+	cartPara.nodeIndexEnd++;
+}
+
+void Cartilage::grow1(double dt) {
+	CVector movementVec = cartPara.growthDirNode1 * cartPara.growthSpeedNode1;
+	uint node1GlobalIndex = cartPara.growNode1Index
+			+ nodes->getAllocPara().startPosCart;
+	nodes->getInfoVecs().nodeLocX[node1GlobalIndex] += movementVec.GetX();
+	nodes->getInfoVecs().nodeLocY[node1GlobalIndex] += movementVec.GetY();
+	nodes->getInfoVecs().nodeLocZ[node1GlobalIndex] += movementVec.GetZ();
+}
+
+void Cartilage::grow2(double dt) {
+	CVector movementVec = cartPara.growthDirNode2 * cartPara.growthSpeedNode2;
+	uint node2GlobalIndex = cartPara.growNode2Index
+			+ nodes->getAllocPara().startPosCart;
+	nodes->getInfoVecs().nodeLocX[node2GlobalIndex] += movementVec.GetX();
+	nodes->getInfoVecs().nodeLocY[node2GlobalIndex] += movementVec.GetY();
+	nodes->getInfoVecs().nodeLocZ[node2GlobalIndex] += movementVec.GetZ();
+}
+
+void Cartilage::runGrowthLogics1(double dt) {
+	uint behind1Index = cartPara.growNodeBehind1Index
+			+ nodes->getAllocPara().startPosCart;
+	CVector node1BehindPos = CVector(
+			nodes->getInfoVecs().nodeLocX[behind1Index],
+			nodes->getInfoVecs().nodeLocY[behind1Index],
+			nodes->getInfoVecs().nodeLocZ[behind1Index]);
+
+	CVector node1GrowthDir = cartPara.growNode1 - node1BehindPos;
+	double distFromBehind1 = node1GrowthDir.getModul();
+	if (distFromBehind1 < cartPara.growthThreshold) {
+		grow1(dt);
+	} else {
+		addPoint1(node1BehindPos);
+		grow1(dt);
+	}
+}
+
+void Cartilage::runGrowthLogics2(double dt) {
+	uint behind2Index = cartPara.growNodeBehind2Index
+			+ nodes->getAllocPara().startPosCart;
+	CVector node2BehindPos = CVector(
+			nodes->getInfoVecs().nodeLocX[behind2Index],
+			nodes->getInfoVecs().nodeLocY[behind2Index],
+			nodes->getInfoVecs().nodeLocZ[behind2Index]);
+
+	CVector node2GrowthDir = cartPara.growNode2 - node2BehindPos;
+	double distFromBehind2 = node2GrowthDir.getModul();
+	if (distFromBehind2 < cartPara.growthThreshold) {
+		grow2(dt);
+	} else {
+		addPoint2(node2BehindPos);
+		grow2(dt);
+	}
+}
+
+void Cartilage::runGrowthLogics(double dt) {
+	runGrowthLogics1(dt);
+	runGrowthLogics2(dt);
+	updateTipNodes();
+}
+
+void Cartilage::initializeNodes(std::vector<CVector>& initFixedNodes,
+		std::vector<CVector>& initTipNodes, CVector& initGrowingNode1,
+		CVector& initGrowingNode2, CVector& initPivotNode1,
+		CVector& initPivotNode2) {
+}
+
+void Cartilage::initializeVal(std::vector<CVector>& initialNodes,
+		CVector& tipNode1, CVector& tipNode2, CVector &pivotNode1,
+		CVector pivotNode2) {
+	uint pivotIndex1 = cartPara.pivotNode1Index
+			+ nodes->getAllocPara().startPosCart;
+	CVector pivotNode1Pos = CVector(nodes->getInfoVecs().nodeLocX[pivotIndex1],
+			nodes->getInfoVecs().nodeLocY[pivotIndex1],
+			nodes->getInfoVecs().nodeLocZ[pivotIndex1]);
+
+	uint pivotIndex2 = cartPara.pivotNode2Index
+			+ nodes->getAllocPara().startPosCart;
+	CVector pivotNode2Pos = CVector(nodes->getInfoVecs().nodeLocX[pivotIndex2],
+			nodes->getInfoVecs().nodeLocY[pivotIndex2],
+			nodes->getInfoVecs().nodeLocZ[pivotIndex2]);
+
+	cartPara.fixedPt = pivotNode1Pos + pivotNode2Pos;
+	cartPara.fixedPt = cartPara.fixedPt / 2;
+}
+
+void Cartilage::updateTipNodes() {
 
 }
 
 void Cartilage::runAllLogics(double dt) {
-
+	calculateTotalTorque();
+	move(dt);
+	runGrowthLogics(dt);
 }
--- src/srcGPU/Cartilage.h
+++ src/srcGPU/Cartilage.h
@@ -17,23 +17,53 @@
 class CartPara {
 public:
 	CVector fixedPt;
-	CVector GrowthDir;
+	CVector growthDir;
+
+	CVector growthDirNode1;
+	CVector growthDirNode2;
 
 	CVector growNode1;
 	CVector growNode2;
 
+	uint pivotNode1Index;
+	uint pivotNode2Index;
+
 	uint growNode1Index;
 	uint growNode2Index;
 
+	uint growNodeBehind1Index;
+	uint growNodeBehind2Index;
+
+	/**
+	 * memory allocation related parameter.
+	 * number of spaces allocated for tip nodes.
+	 * The are designed to fit the first part of Cart nodes.
+	 */
+	uint tipNodeIndexTotal;
+
+	/**
+	 * memory allocation related parameter.
+	 * should be the same with the corresponding parameter in SceNodes.
+	 */
+	uint totalAvailableIndicies;
+
+	/**
+	 * this value changes with the cartilage grows.
+	 */
+	uint nodeIndexEnd;
+
 	double growthSpeedNode1;
 	double growthSpeedNode2;
+
 	/**
 	 * moment of intertia.
 	 */
 	double moInertia;
 	//uint activeCartilageNodeCount;
 
-	double totalGrowthSinceLastAdd;
+	double totalTorque;
+	double angularSpeed;
+
 	double growthThreshold;
 };
 
@@ -41,16 +71,46 @@ class Cartilage {
 	SceNodes* nodes;
 	CartPara cartPara;
 
-	std::vector<uint> tipIndicies;
+	/**
+	 * the first two will be mainly for the two ends.
+	 */
+	std::vector<uint> tipNodeIndicies;
 
 	void calculateGrowthDir();
-	void addPoint();
-	void grow(double dt);
+	void addPoint1(CVector &nodeBehindPos);
+	void addPoint2(CVector &nodeBehindPos);
+	void grow1(double dt);
+	void grow2(double dt);
+	void runGrowthLogics1(double dt);
+	void runGrowthLogics2(double dt);
+	void updateTipNodes();
+	void runGrowthLogics(double dt);
 	void calculateTotalTorque();
 	void move(double dt);
+
+	/**
+	 * Initialize the values using processed information.
+	 * Need to change isActive and
+	 */
+	void initializeNodes(std::vector<CVector> &initFixedNodes,
+			std::vector<CVector> &initTipNodes, CVector &initGrowingNode1,
+			CVector &initGrowingNode2, CVector& initPivotNode1,
+			CVector& initPivotNode2);
+
 public:
 	Cartilage();
-	void initialize(SceNodes* nodeInput);
+	void initializeMem(SceNodes* nodeInput);
+
+	/**
+	 * Initialize the values.
+	 * @param initialNodes contains positions of all initial nodes
+	 * @param tipNode1 is the location of node1 on the tip of growth
+	 * @param tipNode2 is the location of node2 on the tip of growth
+	 * @param pivotNode1 is used for determine the fixed point
+	 * @param pivotNode2 is used for determine the fixed point
+	 */
+	void initializeVal(std::vector<CVector> &initialNodes, CVector &tipNode1,
+			CVector &tipNode2, CVector &pivotNode1, CVector pivotNode2);
 	void runAllLogics(double dt);
 };
 
