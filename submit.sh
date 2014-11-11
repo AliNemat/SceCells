1710adba3ed33088163fee8efba7a182df2a57ec
Wenzhao Sun <wsun2@acms-gpu.math.nd.edu>
2014-11-10 23:04:55 -0500

    prepare for batch submission

---
+++ submit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -o myjob.%j.%N.out 
+##SBATCH -D 
+#SBATCH -J tutorial_1 
+#SBATCH --ntasks=1 
+#SBATCH --mail-type=end 
+#SBATCH --mail-user=@nd.edu 
+#SBATCH --time=00:01:00 
+#SBATCH --nodes=1 
+#SBATCH --ntasks-per-node=1
+#SBATCH --share
+
+sleep 10
+srun -l ./bin/runDiskSimulation
