faff8dfb2f3c83c7843037471faeb14a8a7be76e
Wenzhao Sun <laosunhust@gmail.com>
2014-11-18 23:49:13 -0500

    added missing cmake file

---
+++ tst/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/wenzhao/cuda-workspace/SceCells/tst/googletest
+# Build directory: /home/wenzhao/cuda-workspace/SceCells/tst/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
